191c9d1cba3cf82f9af693c3e9925a863647420d
Tyler Hunt <tyler.s.hunt05@gmail.com>
2024-12-19 12:28:42 -0800

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "kms_access" {
   # checkov:skip=CKV_AWS_111: todo reduce perms on key
   # checkov:skip=CKV_AWS_109: ADD REASON
   statement {
-    sid = "KMS Key Default"
+    sid = "KMSKeyDefault"
     principals {
       type        = "AWS"
       identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
